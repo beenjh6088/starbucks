05af09ccd6bc1ae79b77d55cca9fc12bc1702a32
aseruna <beenjh6088@gmail.com>
2021-05-30 20:28:10 +0900

    visual, notice added

--- js/common.js
+++ js/common.js
@@ -17,4 +17,47 @@ searchInputEl.addEventListener('focus', function(){
 searchInputEl.addEventListener('blur', function(){
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
-})
+});
+
+
+
+//배지 요소 찾기
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function(){
+  if(window.scrollY > 500){
+    //배지 숨기기
+    //gsap.to(요소, 초단위 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  }else{
+    //배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
+// _.throttle(함수, 시간)
+
+
+
+// 순차적 fade-in 기능 추가
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, index){
+  gsap.to(fadeEl, 1, {
+    delay: (index+1)*.7,
+    opacity: 1
+  });
+});
+
+
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+});
