3d2b498bd16bc77e66651dc672ab2bdd59066c2c
aseruna <beenjh6088@gmail.com>
2021-06-03 05:49:26 +0900

    rewards, youtube, season-product, reserve-coffee, pick-your-favorite added

---
+++ js/main.js
@@ -0,0 +1,119 @@
+//검색창 요소 찾기
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+//검색창 요소를 클릭하면 실행
+searchEl.addEventListener('click', function(){
+  searchInputEl.focus();
+});
+
+//검색창 요소 내부 실제 input 요소에 포커스되면 실행.
+searchInputEl.addEventListener('focus', function(){
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+//검색창 요소 내부 실제 input 요소에서 포커스가 해제(블러)되면 실행
+searchInputEl.addEventListener('blur', function(){
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
+
+
+
+//배지 요소 찾기
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function(){
+  if(window.scrollY > 500){
+    //배지 숨기기
+    //gsap.to(요소, 초단위 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  }else{
+    //배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
+// _.throttle(함수, 시간)
+
+
+
+// 순차적 fade-in 기능 추가
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, index){
+  gsap.to(fadeEl, 1, {
+    delay: (index+1)*.7,
+    opacity: 1
+  });
+});
+
+
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+});
+new Swiper('.promotion .swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+
+
+//프로모션 토글 기능
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function(){
+  isHidePromotion = !isHidePromotion;
+  if(isHidePromotion){
+    // 숨김 처리
+    promotionEl.classList.add('hide');
+  }else{
+    // 보임 처리
+    promotionEl.classList.remove('hide');
+  }
+});
+
+
+
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+//이미지에 플로팅 기능 추가
+function floatingObject(selector, delay, size) {
+  gsap.to(selector, random(1.5, 2.5), {
+    y: size,
+    repeat: -1,
+    yoyo: true,
+    ease: Power1.easeInOut,
+    delay: random(0, delay)
+  });
+}
+floatingObject('.floating', 1, 15);
+floatingObject('.floating', .5, 15);
+floatingObject('.floating', 1.5, 20);
